80581b8788e13e5cdcd7efc6e35dd76def188f91
kiel-h-byrne <kiel.byrne@gmail.com>
2023-05-24 01:53:23 -0400

    .

--- index.js
+++ index.js
@@ -12,24 +12,23 @@ const octo = new Octokit({
 
 async function main() {
   envCheck();
-  // const json = await getTopTracks();
   const json = await getPlaylist();
   console.log(json);
-  // await updateTopTracks(json);
+  await updateWithPlaylistTracks(json);
 }
 
 async function envCheck() {
-  // if (!github_token || !gist_id) {
-  //   throw new Error(
-  //     `
-  //       spotify-box ran into an issue for getting your Environment Secrets
-  //       Please make sure you have the following Environment Secrets set:
-  //         GH_TOKEN
-  //         GIST_ID
-  //       For more information, see the README.md: https://github.com/izayl/spotify-box#-environment-secrets
-  //     `
-  //   );
-  // }
+  if (!github_token || !gist_id) {
+    throw new Error(
+      `
+        spotify-box ran into an issue for getting your Environment Secrets
+        Please make sure you have the following Environment Secrets set:
+          GH_TOKEN
+          GIST_ID
+        For more information, see the README.md: https://github.com/izayl/spotify-box#-environment-secrets
+      `
+    );
+  }
 }
 
 async function updateTopTracks(json) {
@@ -84,7 +83,59 @@ async function updateTopTracks(json) {
     throw error;
   }
 }
+async function updateWithPlaylistTracks(json) {
+  let gist;
+  try {
+    gist = await octo.gists.get({
+      gist_id,
+    });
+  } catch (error) {
+    console.error(
+      `spotify-box ran into an issue for getting your gist ${gist_id}:\n${error}`
+    );
+    throw error;
+  }
+  const playlistName = json.name
+  //json = {name: 'My Top Tracks', tracks: {items: [{name: ''}]}}
+  const tracks = json.tracks.items.map((item) => ({
+    name: item.name,
+    artist: item.artists
+      .map((artist) => artist.name.trim())
+      .join(' & '),
+  }));
+  if (!tracks.length) return;
 
+  const lines = [];
+  for (let index = 0; index < Math.min(tracks.length, 10); index++) {
+    let { name, artist } = tracks[index];
+    name = truncate(name, 25);
+    artist = truncate(artist, 19);
+
+    const line = [
+      name.padEnd(34 + name.length - eaw.length(name)),
+      artist.padStart(20 + artist.length - eaw.length(artist)),
+    ];
+    lines.push(line.join(''));
+  }
+
+  try {
+    const filename = Object.keys(gist.data.files)[0];
+    await octo.gists.update({
+      gist_id,
+      files: {
+        [filename]: {
+          filename: `🎵 My Study Playlist in Spotify - ${playlistName}`,
+          content: lines.join('\n'),
+        },
+      },
+    });
+  } catch (error) {
+    console.error(
+      `spotify-box ran into an issue for updating your gist:\n${error}`
+    );
+    throw error;
+  }
+}
 function truncate(str, len) {
   // string longer than `len`
   for (let i = len - 2; i >= 0; i--) {
